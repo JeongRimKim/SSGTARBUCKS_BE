380247a92348c9aa2bf274eeebd7a66a16e49708
kim-jung-yun <jungyun5535@gmail.com>
2024-02-01 05:12:47 +0900

    feat : 비밀번호 변경

--- src/main/java/com/ssgtarbucks/MainApplication.java
+++ src/main/java/com/ssgtarbucks/MainApplication.java
@@ -3,23 +3,15 @@ package com.ssgtarbucks;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.PropertySource;
 
 
 @SpringBootApplication
 @PropertySource(value = "classpath:custom.properties")
-public class MainApplication  extends SpringBootServletInitializer  {
+public class MainApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MainApplication.class, args);
 	}
-	
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-		return builder.sources(MainApplication.class);
-	}
-
 
 }
--- src/main/java/com/ssgtarbucks/controller/ProductController.java
+++ src/main/java/com/ssgtarbucks/controller/ProductController.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,8 +17,6 @@ import com.ssgtarbucks.service.ProductService;
 
 @RestController
 @RequestMapping("/api/v1/product")
-@CrossOrigin("*")
-
 public class ProductController {
 	
 	@Autowired
